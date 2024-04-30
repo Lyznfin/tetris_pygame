8e08f85be56c3abcb4848278c74b56b4bc62bae5
manzil.akbar@gmail.com <manzil.akbar@gmail.com>
2024-04-30 08:02:31 +0700

    Colored screen

--- main.py
+++ main.py
@@ -12,11 +12,17 @@ pygame.display.set_caption("Tetris")
 # Create clock to control framerate | like timeDelta or sum
 CLOCK = pygame.time.Clock()
 
+# Making color
+DARK_BLUE = (44, 44, 162) # RGB something something
+
+# Fill screen with a color
+SCREEN.fill(DARK_BLUE)
+
 # Create game loop
 while True:
     # Event handling
     for event in pygame.event.get():
-        
+
         # Check for quit event
         if event.type == pygame.QUIT:
             pygame.quit()

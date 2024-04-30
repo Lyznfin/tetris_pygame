3fd9e14dc022fe3ae42f641252370a8be021f64c
manzil.akbar@gmail.com <manzil.akbar@gmail.com>
2024-04-30 07:58:52 +0700

    Added empty display and game loop

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-import pygame
+import pygame, sys
+
+# Initialize pygame
+pygame.init()
+
+# Create game windows
+WIDTH = 450
+HEIGTH = 750
+SCREEN = pygame.display.set_mode((WIDTH, HEIGTH))
+pygame.display.set_caption("Tetris")
+
+# Create clock to control framerate | like timeDelta or sum
+CLOCK = pygame.time.Clock()
+
+# Create game loop
+while True:
+    # Event handling
+    for event in pygame.event.get():
+        
+        # Check for quit event
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Updates display
+    pygame.display.update()
+
+    # Framerate | run how many times per second
+    CLOCK.tick(90)
